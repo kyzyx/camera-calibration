1aebc3b20579f4a35025f0835a73eca9ddc7f42e
Edward Zhang <edzhang@cs.washington.edu>
2014-05-23 05:30:11 -0400

    Updates to raw->png converter

--- convert.py
+++ convert.py
@@ -6,19 +6,23 @@ import sys
 from subprocess import call
 
 if len(sys.argv) < 5:
-    print "Usage: python convert.py n_frames frames_per_exposure delta_exposure initial_exposure"
+    print "Usage: python convert.py n_frames frames_per_exposure delta_exposure initial_exposure [start frame]"
 else:
     n = int(sys.argv[1]) # Total number of frames to process
     fpe = int(sys.argv[2]) # Number of frames per exposure
     de = float(sys.argv[3]) # Exposure increment
     initial = float(sys.argv[4]) # Initial exposure
+    start = 0
+    if len(sys.argv) > 5:
+        start = int(sys.argv[5])
 
     outputfile = "imagelist.txt"
 
     o = open(outputfile, "w")
-    for idx,i in enumerate(range(0,n,fpe)):
-        fname = "image%3d.png"%(idx)
+    for idx,i in enumerate(range(start,start+n,fpe)):
+        fname = "image%03d.png"%(idx)
         files = ["RGB:Color_%d.raw"%x for x in range(i,i+fpe)]
         cmd = ["convert", "-size", "640x480", "-depth", "8"] + files + ["-average", fname]
+        print ' '.join(cmd)
         call(cmd)
         o.write("%s %f\n"%(fname, initial+idx*de))
